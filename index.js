a1876f54a5e8988a13f97667fd1492add858d42e
FaRusDev <22978769+FaRusDev@users.noreply.github.com>
2025-03-23 13:00:00 +0700

    feat: simpan di local storage

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+document.addEventListener("DOMContentLoaded", loadTask)
+
 function addTask() {
   let input = document.getElementById("taskInput")
   let taskTes = input.value.trim()
@@ -7,19 +9,66 @@ function addTask() {
   }
 
   let li = document.createElement("li")
-  li.innerHTML = `<span onclick={toggleComplete(this)}>${taskTes}</span><button class="remove" onclick="deleteTask(this)">x</button>`
+  li.innerHTML = `<span onclick="toggleComplete(this)">${taskTes}</span><button class="remove" onclick="deleteTask(this, '${taskTes}')">x</button>`
 
   document.getElementById("taskList").appendChild(li)
+  saveTask(taskTes)
   input.value = ""
-
-  console.log("inputan : ", taskTes)
 }
 
-function deleteTask(button) {
+function deleteTask(button, taskTes) {
+  let tasks = getTaskFromStorage()
+  tasks = tasks.filter((task) => task.text !== taskTes)
+
   let li = button.parentElement
   li.remove()
+  localStorage.setItem("tasks", JSON.stringify(tasks))
 }
 
 function toggleComplete(span) {
   span.classList.toggle("completed")
+  updateTask(span)
+}
+
+function saveTask(kumpulan) {
+  let tasks = getTaskFromStorage()
+  tasks.push({ text: kumpulan, completed: false })
+  localStorage.setItem("tasks", JSON.stringify(tasks))
+}
+
+function getTaskFromStorage() {
+  let tasks = localStorage.getItem("tasks")
+  try {
+    tasks = JSON.parse(tasks) || []
+  } catch (error) {
+    tasks = []
+  }
+  return tasks
+}
+
+function loadTask() {
+  let tasks = getTaskFromStorage()
+
+  tasks.forEach((task) => {
+    let li = document.createElement("li")
+    li.innerHTML = `<span onclick="toggleComplete(this)" class="${
+      task.completed ? "completed" : ""
+    }">${task.text}</span><button class="remove" onclick="deleteTask(this, '${
+      task.text
+    }')">x</button>`
+    document.getElementById("taskList").appendChild(li)
+  })
+
+  console.log("Tasks: ", tasks)
+}
+
+function updateTask(span) {
+  let tasks = getTaskFromStorage()
+  tasks.forEach((task) => {
+    if (task.text === span.innerHTML) {
+      task.completed = !task.completed
+    }
+  })
+  localStorage.setItem("tasks", JSON.stringify(tasks))
+  console.log("Tasks: ", tasks)
 }
